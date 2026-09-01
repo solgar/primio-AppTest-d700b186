1b5874bfddea933812cf6c3ddec60131a5f34e05
Primio <noreply@primio.dev>
2026-09-01 15:09:26 +0000

    build: update targetSdk to 36
    Explicitly set the target Android SDK version to 36 instead of using the Flutter default.
    
    Primio-revision: 1f181dd8-c78d-6280-82c6-b86c025ae18b

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26 // Android 8.0
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
